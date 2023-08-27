f72512ae93963073936f8541949e9eb82b6d20aa
ravi9443 <115891580+ravi9443@users.noreply.github.com>
2023-08-28 02:42:22 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,110 @@
+// import java.io.*;
+// public class Task1{
+//     int i=0;
+//     Console con=System.console();
+//         public void check()
+//     {
+//          System.out.println("You wants to play more?");
+//             System.out.println("Press 1 to continue or 0 for exit");
+//             int f=Integer.parseInt(con.readLine());
+//             if(f==1){
+//             i=4;
+//             }
+//             else
+//             i=0;
+//             return;
+//     }
+// public static void main(String[] args) {
+    
+//     int temp=(int)(Math.random()*100);
+//     int cnt=0;
+    
+
+//     for( i=3;i>=0;i--)
+//     {
+//         System.out.println("Enter the guess no");
+//         int no=Integer.parseInt(con.readLine());
+//         if(no==temp)
+//         {
+//             System.out.println("Your guess is right");
+            
+//             cnt++;
+//             int f=Integer.parseInt(con.readLine());
+//             // if(f==1)
+//             // {
+//             //     if(i==0)
+//             //     i=3;
+//             //     continue;
+//             // }
+//             // else if(f!=0)
+//             // break;
+//             check();
+//         }
+//         else if(no>temp)
+//         {
+//             System.out.println("Your guess is too high");
+//             System.out.println("now you left with "+i+" choices");
+//         }
+
+//         else if(no<temp)
+//         {
+//             System.out.println("Your guess is too low");
+//             System.out.println("now you left with "+i+" choices");
+//         }
+//         if(i==0)
+//         {
+//            check();
+//         }
+//     }
+//     System.out.println("The Total no. of time you won is "+ cnt);
+
+// }
+// }
+import java.io.*;
+public class Task1 {
+    int i = 0;
+    Console con = System.console();
+
+    public void check() {
+        System.out.println("You wants to play more?");
+        System.out.println("Press 1 to continue or 0 for exit");
+        int f = Integer.parseInt(con.readLine());
+        if (f == 1) {
+            i = 4;
+        } else {
+            i = -1;
+        }
+        return;
+    }
+    
+    public static void main(String[] args) {
+        Task1 game = new Task1(); // Create an instance of Task1
+
+        int temp = (int) (Math.random() * 100);
+        int cnt = 0;
+        int loss=0;
+        for (game.i = 3; game.i >= 0; game.i--) {
+            System.out.println("Enter the guess no");
+            int no = Integer.parseInt(game.con.readLine());
+            if (no == temp) {
+                System.out.println("Your guess is right");
+
+                cnt++;
+                //int f = Integer.parseInt(game.con.readLine());
+                game.check();
+            } else if (no > temp) {
+                System.out.println("Your guess is too high");
+                System.out.println("now you left with " + game.i + " choices");
+                loss++;
+            } else if (no < temp) {
+                System.out.println("Your guess is too low");
+                System.out.println("now you left with " + game.i + " choices");
+                loss++;
+            }
+            if (game.i == 0) {
+                game.check();
+            }
+        }
+        System.out.println("The Total no. of times you won is " + cnt+" The Total no. of times you loss is "+loss);
+    }
+}
